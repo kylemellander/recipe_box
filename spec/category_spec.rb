5d4061e6da79a255c38727640bf0bde9550e27ed
Josue Valverde & Kyle Mellander <student@epicodus.com>
2015-08-26 10:35:12 -0700

    add code for index page.

--- spec/category_spec.rb
+++ spec/category_spec.rb
@@ -4,7 +4,13 @@ describe(Category) do
 
   it('will link a category to a recipe') do
     recipe_test = Recipe.create({:name => "Cyanide and Happiness Casserole", :instructions => "Mix Cyanide with some Noodles; Boom, you're done"})
-    category_test = Category.new({:tag => 'Quick Recipes', :recipe_ids => [recipe_test.id]})
+    category_test = Category.create({:tag => 'Quick Recipes', :recipe_ids => [recipe_test.id]})
+    expect(category_test.recipes()).to(eq([recipe_test]))
+  end
+
+  it("will link a category to a recipe through other methods") do
+    recipe_test = Recipe.create({:name => "Cyanide and Happiness Casserole", :instructions => "Mix Cyanide with some Noodles; Boom, you're done"})
+    category_test = recipe_test.categories.create({tag: "Quick Recipes"})
     expect(category_test.recipes()).to(eq([recipe_test]))
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ set(:root, Dir.pwd())
 
 Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each { |file| require file }
 
-# require('capybara/rspec')
-# Capybara.app = Sinatra::Application
-# set(:show_exceptions, false)
-# require('./app')
+require('capybara/rspec')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+require('./app')
