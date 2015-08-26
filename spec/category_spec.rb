6085f5fa7a062bc1781dd4cf6ba75dc10d0ce074
Josue Valverde & Kyle Mellander <student@epicodus.com>
2015-08-26 09:49:27 -0700

    add name column to recipes; add and pass spec that links a newly created category to an existing recipe.

--- spec/category_spec.rb
+++ spec/category_spec.rb
@@ -3,6 +3,8 @@ require('spec_helper')
 describe(Category) do
 
   it('will link a category to a recipe') do
-    
+    recipe_test = Recipe.create({:name => "Cyanide and Happiness Casserole", :instructions => "Mix Cyanide with some Noodles; Boom, you're done"})
+    category_test = Category.new({:tag => 'Quick Recipes', :recipe_ids => [recipe_test.id]})
+    expect(category_test.recipes()).to(eq([recipe_test]))
   end
 end
