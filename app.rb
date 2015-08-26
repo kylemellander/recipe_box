0fb1f5b5407cca83c9502360fff427ea65b33ded
Josue Valverde & Kyle Mellander <student@epicodus.com>
2015-08-26 14:48:40 -0700

    add functionality to add new recipes to existing tags.

--- app.rb
+++ app.rb
@@ -17,10 +17,21 @@ post('/recipes/new') do
   name = params['name']
   instructions = params['instructions']
   @tag_string = params['tag']
-  @tag = Category.new({:tag  => @tag_string})
-  @recipe = Recipe.new({:name => name, :instructions => instructions})
-  params['tag'].separate_tags(@recipe)
-  if @recipe.save && @tag.valid?
+  @tag = Category.new({tag: @tag_string.gsub(/[, ]/, '')})
+  all_categories = Category.all
+  @recipe = Recipe.new({name: name, instructions: instructions})
+  tags = params['tag'].gsub(/, /, ',').split(",")
+  if @recipe.valid? && @tag.valid?
+    existing_tag_ids = []
+    tags.each do |tag|
+      if Category.find_by(tag: tag) != nil
+        existing_tag_ids.push(Category.find_by(tag: tag).id)
+      else
+        @recipe.categories.push(Category.new({tag: tag}))
+      end
+    end
+    @recipe.update({:category_ids => existing_tag_ids})
+    @recipe.save
     redirect("/recipes/#{@recipe.id}")
   else
     erb(:add_recipe)
--- lib/category.rb
+++ lib/category.rb
@@ -3,13 +3,3 @@ class Category < ActiveRecord::Base
   validates(:tag, :presence => true)
 
 end
-
-class String
-  define_method(:separate_tags) do |recipe_to_add_to|
-    tags = self.gsub(/, /, ',').split(",")
-
-    tags.each do |tag|
-      recipe_to_add_to.categories.push(Category.new({:tag => tag}))
-    end
-  end
-end
