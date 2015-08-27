152dd241320c479e0a8888aa4982fe42cceeb7c7
Josue Valverde & Kyle Mellander <student@epicodus.com>
2015-08-27 12:27:15 -0700

    add fetch for edited ingredients without duplication.

--- app.rb
+++ app.rb
@@ -68,9 +68,12 @@ end
 
 patch('/recipes/:id') do
   name = params['name']
+  ingredients = params['ingredients']
+  amounts = params['amounts']
   instructions = params['instructions'].gsub(/\n/, "<br>")
   tags = params['tags'].gsub(/, /, ",").split(',')
   recipe = Recipe.find(params['id'].to_i)
+
   existing_tag_ids = []
   tags.each do |tag|
     if Category.find_by(:tag => tag) != nil
@@ -80,7 +83,18 @@ patch('/recipes/:id') do
       existing_tag_ids.push(Category.find_by(tag: tag).id)
     end
   end
-  recipe.update({:category_ids => existing_tag_ids, name: name, instructions: instructions})
+
+  existing_ingredient_ids = []
+  ingredients.each do |ingredient|
+    if Ingredient.find_by(:ingredient => ingredient.ingredient) != nil
+      existing_ingredient_ids.push(Ingredient.find_by(:ingredient => ingredient.ingredient).id)
+    else
+      ingredient_object = Ingredient.create({:ingredient => ingredient.ingredient})
+      existing_ingredient_ids.push(Ingredient.find_by(:ingredient => ingredient.ingredient).id)
+    end
+  end
+
+  recipe.update({:category_ids => existing_tag_ids, :ingredient_ids => existing_ingredient_ids, name: name, instructions: instructions})
   redirect("/recipes/#{recipe.id}")
 end
 
@@ -106,4 +120,14 @@ get('/category/:id/delete') do
   category = Category.find(params['id'].to_i)
   category.destroy
   redirect("/")
+end
+
+get('/ingredients/delete/:id/') do
+  recipe_id = params['ref'].to_i
+  ingredient_id = params['id'].to_i
+  ingredient = Ingredient.find(ingredient_id)
+  used_ingredient = UsedIngredient.find_by(recipe_id: recipe_id, ingredient_id: ingredient_id)
+  used_ingredient.destroy
+  ingredient.destroy
+  redirect("/recipe/#{recipe_id}/edit")
 end
